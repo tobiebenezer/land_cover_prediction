0055dfffefe9a0809017b3a51271638a4978d870
Tobi Ebenezer AWODUMILA <tobi@aims.ac.za>
2024-09-20 15:53:49 +0200

    setup training

--- model/base.py
+++ model/base.py
@@ -12,28 +12,30 @@ device = (
 
 class MBase(nn.Module):
     def training_step(self,batch):
-        X, y = batch
+        X, (y ,_)= batch
         out = self(X.to(device))
-        loss = F.mse_loss(out,out) # calculating loss
-        print(out,y)
+        loss = F.mse_loss(out,y.to(device)) # calculating loss
+      
         return loss
     
     def validation_step(self, batch):
-        X, y = batch
+        X,( y,_) = batch
         out = self(X.to(device))
-        loss = F.mse_loss(out,y)
+        loss = F.mse_loss(out,y.to(device))
         acc = accuracy(out, y.to(device))
         return {'val_loss':loss.detach(), 'val_accuracy':acc}
     
     def validation_epoch_end(self, outputs):
         batch_losses = [x['val_loss'] for x in outputs]
+        batch_accuracy = [x['val_accuracy'] for x in outputs]
         epoch_loss = torch.stack(batch_losses).mean()
-        return {'val_loss': epoch_loss.item()}
+        epoch_acc = torch.stack(batch_accuracy).mean()
+        return {'val_loss': epoch_loss.item(),'val_accuracy':epoch_acc.item()}
     
     def epoch_end(self, epoch, result):
-        print("Epoch [{}], train_loss: {:.4f}, val_loss: {:.4f}".format(
-            epoch, result['train_loss'], result['val_loss']))
+        print("Epoch [{}], train_loss: {:.4f}, val_loss: {:.4f}, val_acc: {:.4f}".format(
+            epoch, result['train_loss'], result['val_loss'], result['val_accuracy']))
         
 
 def accuracy(outputs, labels):
-    return torch.mean(torch.abs((labels - outputs) / labels)) * 100
+    return torch.mean(torch.abs((labels - outputs))) * 100
