83570c8ffdce229d871803d6c52fb3b21bf8b8dd
Tobi Ebenezer AWODUMILA <tobi@aims.ac.za>
2024-09-21 23:42:28 +0200

    fix scaler

--- triain_tokenizer.py
+++ triain_tokenizer.py
@@ -43,9 +43,9 @@ else:
 test_dataset = PatchDataset(ndvi_3d,mode="test",scaler=scaler)
 val_dataset = PatchDataset(ndvi_3d,mode="val",scaler=scaler)
 
-train_dataloader = DataLoader(train_dataset,batch_size=4, shuffle=False)
-val_dataloader = DataLoader(val_dataset,batch_size=4, shuffle=False)
-test_dataloader = DataLoader(test_dataset,batch_size=4, shuffle=False)
+train_dataloader = DataLoader(train_dataset,batch_size=4, shuffle=True)
+val_dataloader = DataLoader(val_dataset,batch_size=4, shuffle=True)
+test_dataloader = DataLoader(test_dataset,batch_size=4, shuffle=True)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='training')
