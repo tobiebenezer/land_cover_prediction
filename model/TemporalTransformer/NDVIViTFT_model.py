62905339f77056387df6ccc31e15b7732033dbac
Awodumila, Tobi Ebenezer <tobi@mail.com>
2024-09-26 21:45:25 +0200

    last change

--- model/TemporalTransformer/NDVIViTFT_model.py
+++ model/TemporalTransformer/NDVIViTFT_model.py
@@ -7,7 +7,13 @@ from model.TemporalTransformer.NDVIViTEncoder import *
 from model.TemporalTransformer.tft import *
 from model.base import MBase
 
-
+device = (
+    "cuda"
+    if torch.cuda.is_available()
+    else "mps"
+    if torch.backends.mps.is_available()
+    else "cpu"
+)
 
 class NDVIViTFT(MBase):
     def __init__(self,
