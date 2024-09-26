26a0e622e4362c8c0dde59f30f2ef2e6847bd884
Awodumila, Tobi Ebenezer <tobi@mail.com>
2024-09-26 02:44:42 +0200

    added NDVIViTEncoder

--- model/TemporalTransformer/module.py
+++ model/TemporalTransformer/module.py
@@ -265,9 +265,10 @@ class PatchEmbedding(nn.Module):
 
     def forward(self, x):
         x = rearrange(x, 'b p h w -> (b p) 1 h w')
+        b, n, _, _ = x.shape
         x = self.proj(x)
         print(x.shape)
-        x = rearrange(x, '(b p) c h w -> b p (c h w)', b=x.shape[0], p=x.shape[1])
+        x = rearrange(x, '(b p) c h w -> b p (c h w)', b=b, p=n)
         return x
 
 
