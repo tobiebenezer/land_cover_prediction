1f6a0a2ff01820e352a1aec71c4127a8beff61cb
Tobi Ebenezer AWODUMILA <tobi@aims.ac.za>
2024-09-27 21:37:56 +0200

    debuging tft

--- model/TemporalTransformer/tft.py
+++ model/TemporalTransformer/tft.py
@@ -102,9 +102,10 @@ class TemporalFusionTransformer(nn.Module):
         lstm_outputs = rearrange(lstm_outputs, "b s (n h) -> b (s n) h", h=self.hidden_size)
         print(lstm_outputs.shape,"lstm_outputs")
         gated_outputs = self.gated_skip_connection(lstm_outputs)
+        gated_outputs = rearrange(gated_outputs, "b (s n) h -> (b s) n h", s=s)
         print(gated_outputs.shape,"gated_outputs")
         print(x.shape,"x")
-        temporal_feature_outputs = self.add_norm(x + gated_outputs)
+        temporal_feature_outputs = self.add_norm(x[:,:-1,:] + gated_outputs)
 
         # static_enrichment_outputs = self.static_enrichment(torch.cat([temporal_feature_outputs, static_context_e.unsqueeze(1).expand(-1, temporal_feature_outputs.size(1), -1)], dim=-1))
 
