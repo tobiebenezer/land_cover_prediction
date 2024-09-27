e2aeaafe5bc17d7b2e935820af372851fa4a5367
Tobi Ebenezer AWODUMILA <tobi@aims.ac.za>
2024-09-27 14:09:25 +0200

    train_TFT

--- model/TemporalTransformer/tft.py
+++ model/TemporalTransformer/tft.py
@@ -44,8 +44,8 @@ class TemporalFusionTransformer(nn.Module):
     # @torch.jit.script
     def define_lstm_encoder(self, x, static_context_h, static_context_c):
         print(x.shape, "lstm encoder input")
-        static_context_h = rearrange(static_context_h, "b (s p) n -> b s (p n)" , p=self.sequence_length)
-        static_context_h = rearrange(static_context_c, "b (s p) n -> b s (p n)" , p=self.sequence_length)
+        static_context_h = rearrange(static_context_h, "(s p) n -> s (p n)" , p=self.sequence_length)
+        static_context_h = rearrange(static_context_c, "(s p) n -> s (p n)" , p=self.sequence_length)
         
         print(static_context_h.shape, "static context")
         output, (state_h, state_c) = self.encoder_lstm(x, (static_context_h.unsqueeze(0).repeat(self.num_layers,1,1),
