ba73d98cd5dab7f9232309144a1400a84faf8738
Tobi Ebenezer AWODUMILA <tobi@aims.ac.za>
2024-09-27 11:06:40 +0200

    train_TFT

--- model/TemporalTransformer/tft.py
+++ model/TemporalTransformer/tft.py
@@ -74,8 +74,8 @@ class TemporalFusionTransformer(nn.Module):
         static_context_e, static_context_h, static_context_c = self.define_static_covariate_encoders(context)
         
         future_size = x.shape[0]//4
-        past_input = x[:, :x.shape[0]-future_size, :, :]
-        future_input = x[:, x.shape[0]-future_size:, :, :]
+        past_input = self.input_embedding(x[:, :x.shape[0]-future_size, :, :])
+        future_input = self.input_embedding(x[:, x.shape[0]-future_size:, :, :])
         print(x.shape,past_input.shape, future_input.shape,"past and future")
      
         encoder_output, state_h, state_c = self.define_lstm_encoder(past_input, static_context_h, static_context_c)       
