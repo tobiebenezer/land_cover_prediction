3dc742e08ea1f9cbbb92596968baa4d292edeb87
Awodumila, Tobi Ebenezer <tobi@mail.com>
2024-10-12 22:41:22 +0200

    fix resnet

--- model/resencoder.py
+++ model/resencoder.py
@@ -1,6 +1,6 @@
 import torch
 import torch.nn as nn
-from torchvision.models import resnet18
+from torchvision.models import resnet18, ResNet18_Weights
 from einops import rearrange
 
 class ResNet18Encoder(nn.Module):
@@ -8,7 +8,8 @@ class ResNet18Encoder(nn.Module):
         super(ResNet18Encoder, self).__init__()
         
         # Load pretrained ResNet18 model
-        resnet = resnet18(pretrained=True)
+        weights = ResNet18_Weights.DEFAULT
+        resnet = resnet18(weights=weights)
         
         # Modify the first convolution layer to accept 1 input channel instead of 3
         self.conv1 = nn.Conv2d(in_channels, 64, kernel_size=7, stride=2, padding=3, bias=False)
